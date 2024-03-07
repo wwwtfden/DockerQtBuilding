d23880dfbb561e6b9f8fe2e4f550394833553623
wwwtfden <denn.tdi@gmail.com>
2024-03-08 00:25:40 +0300

    Updated get data script

--- get_data.sh
+++ get_data.sh
@@ -1,3 +1,4 @@
 docker create --name tempqt6 qt6build
 mkdir qt6_binaries
-docker cp tempqt6:/workspace/qt-everywhere-src-6.6.1/build qt6_binaries
+#docker cp tempqt6:/workspace/qt-everywhere-src-6.6.1/build qt6_binaries
+docker cp tempqt6:/Qt6.6.1 qt6_binaries
